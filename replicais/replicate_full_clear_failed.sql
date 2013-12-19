f92ae4706ec44a3d68446efe09a4eb19dcac6853
Alexey Grishchenko <programmer.a.g@gmail.com>
2013-12-19 16:27:18 +0400

    one more fuckup
    Why left join? To kill all running replication tasks for sure?

--- replicais/replicate_full_clear_failed.sql
+++ replicais/replicate_full_clear_failed.sql
@@ -1,18 +1,22 @@
-create or replace function replicais.replicate_full_clear_failed() returns void as $$
+create or replace function replicais.replicate_full_clear_failed() returns void as $BODY$
 declare
     v_tasks_list    varchar[];
     v_table_list    varchar[];
 begin
     select array_agg(r.replication_task_isn), array_agg(r.replication_table)
         into v_tasks_list, v_table_list
-        from (select replication_task_isn, replication_table, replication_job_isn
-                from replicais.replication_tasks_full
-                where replication_job_isn is not null and replication_status = 'RUN') as r 
-                    left join 
-                        (select job_isn
-                            from dbms_jobs.job_queue
-                            where job_status = 'FLD') as j
-                    on r.replication_job_isn = j.job_isn;
+        from (
+                select replication_task_isn, replication_table, replication_job_isn
+                    from replicais.replication_tasks_full
+                    where replication_job_isn is not null
+                        and replication_status = 'RUN'
+            ) as r 
+            inner join (
+                select job_isn
+                    from dbms_jobs.job_queue
+                    where job_status = 'FLD'
+            ) as j
+            on r.replication_job_isn = j.job_isn;
     if array_upper(v_tasks_list, 1) > 0 then
         update replicais.replication_tasks_full
             set replication_status = 'FLD'
@@ -20,5 +24,6 @@ begin
         perform shared_system.send_email ('Full replication failed', 'Failed full replication for tables ' || array_to_string(v_table_list, ', ') || '. Related ISNs are: , '||array_to_string(v_tasks_list, ', '));
     end if;
 end;
-$$language plpgsql
+$BODY$
+language plpgsql
 volatile;
